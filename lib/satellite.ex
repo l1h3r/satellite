b3fc3012c9f469f9fd99dd6f13b0b7b8181d49fb
l1h3r <devin.turner09@gmail.com>
2018-03-28 20:28:32 -0700

    inital commit

---
+++ lib/satellite.ex
@@ -0,0 +1,18 @@
+defmodule Satellite do
+  @moduledoc """
+  Documentation for Satellite.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Satellite.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
